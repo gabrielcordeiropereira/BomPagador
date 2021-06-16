97dc4f40e66a93d5a649d645cc504efce5b36f7f
gabrielcordeiropereira <cordeiro.gabriel@engenharia.ufjf.br>
2021-06-15 21:55:04 -0300

    Contexto, DbContext FornecedorMap

---
+++ BomPagador/Data/Map/FornecedorMap.cs
@@ -0,0 +1,34 @@
+﻿using BomPagador.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BomPagador.Data.Map
+{
+    public class FornecedorMap : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.ToTable("Fornecedor");
+
+            builder.Property(x => x.Nome)
+                .HasColumnType("varchar(70)")
+                .IsRequired();
+
+            builder.Property(x => x.Link)
+                .HasColumnType("varchar(150)");
+
+            builder.Property(x => x.Cnpj)
+                .HasColumnType("varchar(25)")
+                .IsRequired();
+
+            builder.Property(x => x.Vertical)
+                .HasColumnType("varchar(50)")
+                .IsRequired();
+                
+        }
+    }
+}
